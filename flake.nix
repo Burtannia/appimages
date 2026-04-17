00508958b8fd88bfe6ecf6640de67b0a9279b735
Burtannia <james@functionalcore.dev>
2026-04-17 15:58:07 +0100

    remove warcraftlogs

--- flake.nix
+++ flake.nix
@@ -15,7 +15,6 @@
     in {
       packages.${system} = {
         raiderio-client = pkgs.callPackage ./raiderio-client.nix {};
-        warcraftlogs = pkgs.callPackage ./warcraftlogs.nix {};
       };
     };
 }
